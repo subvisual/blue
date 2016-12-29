e2e8a5c599b09087b109277f5beb59d37686ee7c
Miguel Palhas <mpalhas@gmail.com>
2016-12-29 12:45:34 +0000

    Upgrading for middleman 4 compatibility

--- javascripts/blue.js
+++ javascripts/blue.js
@@ -1,6 +1,6 @@
-//= require ./blue/_scroll
-//= require ./blue/_scroll_state
-//= require ./blue/_nav_logo
-//= require ./blue/_nav_visibility
-//= require ./blue/_nav
-//= require ./blue/_nav_state
+//= require ./blue/scroll
+//= require ./blue/scroll_state
+//= require ./blue/nav_logo
+//= require ./blue/nav_visibility
+//= require ./blue/nav
+//= require ./blue/nav_state
